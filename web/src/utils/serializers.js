4ca58e9116987c47961d25ea6ff5f2f65414e14d
Charlotte Cameron <charlotte.cameron@adenin.com>
2023-07-13 23:28:54 +0100

    Update alignment block styling

--- web/src/utils/serializers.js
+++ web/src/utils/serializers.js
@@ -22,7 +22,7 @@ const serializers = {
       return tableStr.slice(0, -1);
     },
     youtube: ({node}) => `<iframe class="w-full aspect-video mb-4" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" width="680" height="380" src="https://www.youtube.com/embed/${node.url.split('?v=')[1]}"></iframe>`,
-    textAlign: ({ node }) => `<div class="align" style="text-align: ${node.alignment}">
+    textAlign: ({ node }) => `<div class="align-${node.alignment}">
     ${BlocksToMarkdown(node.text, { serializers, ...client.config() })}
     </div>`,
   },
